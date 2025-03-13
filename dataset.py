ed3f30cc5b8d82d2aa20a2f9de1b7326c93944ee
Ashish Kumar Jha <76522103+ashkjha1@users.noreply.github.com>
2025-03-13 11:39:56 +0530

    Create dataset.py

---
+++ dataset.py
@@ -0,0 +1,18 @@
+# Dataset for vector DB
+youtube_urls = [
+    "https://www.youtube.com/watch?v=oLC2M8ybhL0&ab_channel=%F0%9D%90%8F%F0%9D%90%AC%F0%9D%90%B2%F0%9D%90%9C%F0%9D%90%A1%F0%9D%90%A2%F0%9D%90%9C%F0%9D%90%84%F0%9D%90%9E%F0%9D%90%AC%F0%9D%90%A1",
+    "https://www.youtube.com/shorts/8l8udF7Vs74",
+    "https://www.youtube.com/shorts/IP0LSmb3tdI",
+    "https://www.youtube.com/shorts/Bz8oOHg0Rrs",
+    "https://www.youtube.com/watch?v=jW_ybtzjq7o&ab_channel=GadgetByte",
+    "https://www.youtube.com/watch?v=WL4RWNptU_Y&ab_channel=Mrwhosetheboss",
+    "https://www.youtube.com/shorts/1NXfkAJolF8",
+]
+
+
+|# Test Dataset
+test_urls = [
+    "https://www.youtube.com/shorts/VXcKhP-vMZc",
+    "https://www.youtube.com/shorts/S7el_hnWgcQ",
+    "https://www.youtube.com/shorts/8jSdxYBgpYw",
+]
